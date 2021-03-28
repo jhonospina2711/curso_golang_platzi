9bf7938ae239922f71f9cc555f45a80d37129c83
jhon ospina <jhonospina2711@gmail.com>
2021-03-28 07:16:19 -0500

    Operadores aritmeticos

--- src/main.go
+++ src/main.go
@@ -3,8 +3,59 @@ package main
 import "fmt"
 
 func main() {
-	//Declaracion de constantes Prueba
-	const pi float64 = 3.15
+	//Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.15
 
-	fmt.Println(pi)
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	//Declaración de variables enteras
+
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//zero values
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Calcular el area de un cuadrado
+
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	//suma
+	result := x + y
+	fmt.Println("Suma: ", result)
+	//Resta
+	result = y - x
+	fmt.Println("Resta: ", result)
+	//Multiplicacion
+	result = x * y
+	fmt.Println("Multiplicacion: ", result)
+	//División
+	result = y / x
+	fmt.Println("Division: ", result)
+	//Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+	//Incremental
+	x++
+	fmt.Println("Incremental: ", x)
+	//Decremental
+	x--
+	fmt.Println("Decremental: ", x)
+
 }
